e6f0561d428c909cf89f01dd28fca0e2c3711883
Sebastian Saner <seb.sa@eblcom.ch>
2023-11-17 17:08:36 +0100

    started attempt constructed type

--- packages/language/src/core/validateDocument.ts
+++ packages/language/src/core/validateDocument.ts
@@ -1,12 +1,16 @@
-import { DocumentProblem, FlowDocument, FlowDocumentContext, FlowEnvironmentNamespace, FlowGraphContext, FlowSignature, LanguageConfiguration } from "../types";
+import _ from "lodash";
+import { createReducedTemplateType, getTemplatedSignatureType, memoizeTemplatedType } from "../typeSystem";
+import { DocumentProblem, FlowDocument, FlowDocumentContext, FlowEnvironmentNamespace, FlowGraphContext, FlowSignature, LanguageConfiguration, TypeSpecifier } from "../types";
 import { ListCache } from "../utils/ListCache";
 import { mem } from '../utils/mem';
 import { createEnvironment } from "./environment";
 import { getFlowSignature, validateFlowGraph } from "./validateFlowGraph";
+import { instantiateTemplatedType } from "../typeSystem/generics";
+import { TypeTreePath } from "../typeSystem/exceptionHandling";
 
 export const validateDocument = mem((document: FlowDocument, configuration: LanguageConfiguration) => {
     const { flows: rawFlowMap } = document;
-    
+
     const flowContexts: Record<string, FlowGraphContext> = {};
     const problems: DocumentProblem[] = [];
     let criticalSubProblems = 0;
@@ -37,14 +41,36 @@ export const validateDocument = mem((document: FlowDocument, configuration: Lang
     tag: 'validateDocument',
 });
 
-const makeDocumentNamespace = mem(
-    (...signatureList: FlowSignature[]): FlowEnvironmentNamespace => ({
-        name: 'document', 
+const makeDocumentNamespace = mem((
+    ...signatureList: FlowSignature[]
+): FlowEnvironmentNamespace => {
+    const outputConstructorTypes: Record<string, TypeSpecifier> = {};
+    for (const signature of signatureList) {
+        const signatureType = getTemplatedSignatureType(signature);
+        const templatedOutput = memoizeTemplatedType(
+            createReducedTemplateType(
+                signatureType.generics,
+                signatureType.specifier.output,
+            )
+        );
+        
+        const dummyEnvironment = createEnvironment({ content: {
+            types: {},
+            signatures: [],
+        }, name: '' });
+        const crudeInstantiatedType = instantiateTemplatedType(
+            new TypeTreePath(), templatedOutput, instantiationMap,
+            templatedOutput.generics, dummyEnvironment);
+        
+        
+        const typeName = _.capitalize(signature.id);
+        outputConstructorTypes[typeName] = templatedOutput;
+    }
+    return {
+        name: 'document',
         content: {
             signatures: signatureList,
             types: {},
         },
-    }),
-    undefined,
-    { tag: 'makeDocumentNamespace' },
-);
+    };
+}, undefined, { tag: 'makeDocumentNamespace' });
